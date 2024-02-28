82c0838f4fe2965c316ee1f2bee74cf2404430ff
Mahendra Patel <email@mahendrapa.tel>
2024-02-28 17:54:45 +0530

    added &pictures

--- m3uparser.py
+++ m3uparser.py
@@ -1,24 +1,34 @@
-from ipytv import playlist
-import sys
+import cv2
+import os
 
-path='mahendra.m3u'
+os.environ["OPENCV_FFMPEG_READ_ATTEMPTS"] = "10000"  # Increase the attempt limit
+def check_video_stream(url):
+    cap = cv2.VideoCapture(url)
+    try:
+        if not cap.isOpened():
+            print("Stream is not active")
+            return
+        
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Stream ended")
+                break
+            
+            # You can optionally display the frame if you want to visually confirm the stream is active
+            cv2.imshow("Frame", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                 break
 
-def safeOpen(path, mode):
-  try:
-    openFile = open(path, mode)
-  except:
-    sys.exit("ERROR: Could not open " + path)
-  return openFile
+        cap.release()
+        cv2.destroyAllWindows()
+        print("Stream is active")
+        
+    except Exception as e:
+        print("Error:", e)
+        cap.release()
+        cv2.destroyAllWindows()
 
-def channel_count(f):
-  pl = playlist.loadf(f)
-  return print(f'There are {pl.length()} channels.')
-
-def list_channels(f, o):
-  outfile = safeOpen(o, 'w')
-  channel_list = []
-  pl = playlist.loadf(f)
-  for channel in pl:
-    outfile.write(channel.name)
-    outfile.write('\n')
-  outfile.close()
+# Example usage
+video_url = "https://feeds.intoday.in/aajtak/api/aajtakhd/master.m3u8"
+check_video_stream(video_url)
